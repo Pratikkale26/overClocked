6088d9513a3facb52764e8adfc7b09f9b34d3d23
Pratik <pratikkale7661@gmail.com>
2026-02-28 06:40:16 +0000

    setup prisma with express server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,25 @@
 import express from "express"
+import { prisma } from "./db";
 
 const app = express();
 app.use(express.json());
+
+app.post("/user", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await prisma.user.create({
+            data: {
+                email,
+                password
+            }
+        })
+        console.log(user);
+        return res.status(201).json(user);
+    }catch (e) {
+        console.log(e);
+        return res.status(500).json({message: "something went wrong"})
+    }
+})
 
 app.listen(8080, () => {
     console.log("api is running on the post 8080")
